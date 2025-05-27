7c61456861fea31daa824359e4487befa5d98318
AlibiAn <alibomaster3@gmail.com>
2025-05-27 17:18:14 +0900

    further

--- mst.cpp
+++ mst.cpp
@@ -14,3 +14,12 @@ double distance(Coordinate a, Coordinate b) {
     double d_y = a.y - b.y;
     return sqrt((d_x*d_x) + (d_y * d_y));
 }
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cout << "Used" << argv[0] << " <tsp_file>" << endl;
+        return -1;
+    }
+
+    return 0;
+}
